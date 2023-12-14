b349d956dae7e9a90c0e247ccb267ebb9942e0e2
Ülkü <93094752+Ulku-dur@users.noreply.github.com>
2023-12-15 01:06:38 +0300

    Üç kenar uzunluğunu kullanıcıdan alınan üçgenin alanını hesaplayan programı yazıldı

---
+++ UcgenAlanCevreHesapla/HipotenusHesapla/src/Main.java
@@ -0,0 +1,21 @@
+
+// Scanner class'ı import edilir;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Üçgenin birinci kenar uzunluğunu girin:");
+        double a = scanner.nextDouble();
+        System.out.println("Üçgenin ikinci kenar uzunluğunu girin:");
+        double b = scanner.nextDouble();
+        System.out.println("Üçgenin üçüncü kenar uzunluğunu girin:");
+        double c = scanner.nextDouble();
+        // Üçgenin çevresini hesapla
+        double u = (a + b + c) / 2;
+        // Alanı hesapla
+        double alan = Math.sqrt(u * (u - a) * (u - b) * (u - c));
+        System.out.println("Üçgenin Çevresi: " + (2 * u));
+        System.out.println("Üçgenin Alanı: " + alan);
+        scanner.close();
+    }
+}
