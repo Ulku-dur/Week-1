73a14e96e94867228570dbb7b59004e4b08934ac
Ülkü <93094752+Ulku-dur@users.noreply.github.com>
2023-12-18 10:49:28 +0300

    kullanıcıdan alınan kriterlere göre uçak bileti fiyatı hesaplanıldı

--- TipDönüşümleri/Casting/src/Main.java
+++ TipDönüşümleri/Casting/src/Main.java
@@ -1,8 +1,7 @@
 /*
-Java'da tip dönüşümlerini anlamak için bir program yazınız.
 Bu program, öncelikle kullanıcıdan bir tam sayı ve bir ondalıklı sayı girmesini istesin. Daha sonra bu tam sayıyı
 ondalıklı sayıya dönüştürerek ve bu ondalıklı sayıyı
-tam sayıya dönüştürerek sonuçları ekrana yazdırsın.
+tam sayıya dönüştürerek sonuçları ekrana yazdırır.
 * */
 
 import java.util.Scanner;
